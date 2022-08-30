d4fa1aa6c78d662a54cd79050d2aa1adc50dc572
Filip <filfer05@gmail.com>
2022-08-30 04:45:57 +0200

    clothing api

--- frontend/src/pages/Clothing.jsx
+++ frontend/src/pages/Clothing.jsx
@@ -9,17 +9,17 @@ export default function Clothing() {
     useEffect(() => {
         let lastPath = location.pathname.split('/').pop()
         axios.get('/clothing/api')
-            .then(res => console.log(res.data))
-            .then(data => lastPath === 'trending' ? data.filter(cloth => cloth.trending) : lastPath === 'new' ? data.filter(cloth => cloth.new) : data.filter(cloth => cloth.sex === lastPath))
-            .then(clothes => setClothes(clothes))
-            .catch(error => console.log([error.message]))
+            .then(res => res.data)
+            .then(data => lastPath === 'trending' ? data.filter(cloth => cloth.trending) : lastPath === 'new' ? data.filter(cloth => cloth.new) : lastPath === 'collection' ? data.filter(cloth => cloth.collection) : lastPath !== 'clothing' ? data.filter(cloth => cloth.sex === lastPath) : data)
+            .then(data => setClothes(data))
+            .catch(error => console.log(error.message))
     }, [location]) 
 
     return (
         <section className="padding-x padding-y">
             <h1 className="font-bold text-2xl">Our {location.pathname === '/clothing/new' ? 'new ' : location.pathname === '/clothing/trending' ? 'trending ' : ''}clothing {location.pathname === "/clothing/men" ? 'for men' : location.pathname === "/clothing/women" ? 'for women' : location.pathname === "/clothing/collection" ? 'collection' : '' }</h1>
             <div className='clothes-wrapper'>
-                <div className='clothes-grid grid'>
+                <div className='clothes-grid grid grid-cols-3 gap-4'>
                     {clothes.map(cloth => <Cloth {...cloth} key={cloth} />)}
                 </div>
             </div>
@@ -30,12 +30,16 @@ export default function Clothing() {
 const Cloth = (props) => {
     return (
         <div>
-            <div className='h-[4in]'>
-                <img className="max-w-[90%] max-h-[90%]" src='' alt='' />
+            <div className='h-[4in] bg-[#BDBDBD]'>
+                <img className="max-w-[90%] max-h-[90%]" src={props.image} alt='' />
             </div>
-            <h3>{props.title}</h3>
-            {props.sale ? <p><s>{props.price}</s></p> : <p>{props.price}</p>}
-            {props.sale ? <p>{props.price * (props.sale / 100)}<strong className='text-red'>{`(-${props.sale}%)`}</strong></p> : {}}
+            <h3 className='text-center my-2'>{props.title}</h3>
+            {props.sale > 0 ? 
+            <>  
+                <p className='text-center'><del>${props.price}</del></p>
+                <p className='text-center'>${props.price - (props.price * (props.sale / 100))}<strong className='text-red'>{`(-${props.sale}%)`}</strong></p>
+            </>
+            : <p className='text-center font-bold'>${props.price}</p>}
         </div>
     )
 }
