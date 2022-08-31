f4563191f02b2fc0841827bc66a31ccfbc60ab3e
Filip <filfer05@gmail.com>
2022-08-31 22:34:55 +0200

    shopping cart functionality

--- frontend/src/reducers/cart.jsx
+++ frontend/src/reducers/cart.jsx
@@ -8,10 +8,14 @@ export const cartSlice = createSlice({
     reducers: {
         add: (state, action) => {
             state.cart.push(action.payload)
+        },
+        remove: (state, action) => {
+            let newCart = state.cart.filter(item => item.id !== action.payload.id)
+            state.cart = newCart
         }
     }
 })
 
-export const { add } = cartSlice.actions
+export const { add, remove } = cartSlice.actions
 
 export default cartSlice.reducer
