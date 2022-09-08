5b08a1c9317dc64510db249d17ce2c6b92b96ff9
Sebastian <se6359@gmail.com>
2022-09-09 00:53:38 +0200

    syf

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -56,6 +56,7 @@ REST_FRAMEWORK = {
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
 
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'corsheaders.middleware.CorsMiddleware',
 
     'django.contrib.sessions.middleware.SessionMiddleware',
