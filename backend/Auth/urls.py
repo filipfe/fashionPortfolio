85a1db2089b66fcce85d754c807ae2bc848b8876
Sebastian <se6359@gmail.com>
2022-09-09 21:15:35 +0200

    url

--- backend/Auth/urls.py
+++ backend/Auth/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('api/user', views.UserView.as_view()),
     path('api/logout', views.LogoutView.as_view()),
     path('api/login/recovery', views.PasswordResetView.as_view(), name='recovery'),
-    path('api/login/recovery/<uidb64>/<token>', views.PasswordTokenCheckAPI.as_view(), name='password-reset-confirm'),
+    path('login/recovery/<uidb64>/<token>', views.PasswordTokenCheckAPI.as_view(), name='password-reset-confirm'),
     path('api/login/recovery/complete', views.NewPasswordAPIView.as_view(), name='complete'),
 ]
