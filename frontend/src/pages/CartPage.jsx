e299b0538ba7efd13841bfdf14ccbc4e4e1bb939
Filip <filfer05@gmail.com>
2022-09-11 04:22:33 +0200

    payment

--- frontend/src/pages/CartPage.jsx
+++ frontend/src/pages/CartPage.jsx
@@ -1,6 +1,7 @@
 import Cart from '../components/Cart'
 import { useLocation } from 'react-router'
 import Shipping from '../components/Shipping'
+import Payment from '../components/Payment'
 
 export default function CartPage() {
     const location = useLocation()
@@ -8,7 +9,7 @@ export default function CartPage() {
     return (
         <section className="padding-y padding-x lg:flex lg:items-center lg:flex-col">
             {url === 'cart' ? <h2 className="font-bold text-2xl lg:text-3xl mb-4">Your cart</h2> : <></>}
-            {url === 'cart' ? <Cart /> : url === 'shipping' ? <Shipping /> : <></>}
+            {url === 'cart' ? <Cart /> : url === 'shipping' ? <Shipping /> : url === 'payment' ? <Payment /> : <></>}
         </section>
     )
 }
