076966e518f36e895d0180ed211a7f622f7b079b
Filip <filfer05@gmail.com>
2022-09-14 01:49:25 +0200

    payment frontend

--- backend/Favourites/urls.py
+++ backend/Favourites/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    path('api/favourites/', views.FavouriteView.as_view()),
+    path('api/favourites', views.FavouriteView.as_view()),
     path('api/favourites/remove/<int:pk>', views.RemoveFavouriteView.as_view()),
     path('api/favourites/id', views.FavouriteUserIdView.as_view()),
 ]
